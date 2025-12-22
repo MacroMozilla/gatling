73e94a947f45f38cddcbbf7a067f8137323e8c2d
harry <hz3866@nyu.edu>
2025-12-21 22:44:54 -0500

    Add SuperFileSystem module and extend hash-based path routing functionality
    - Introduce `sfs_helper.py` and `sfs_main.py` in `h_file_system` for SuperFileSystem implementation.
    - Add `PathRouter` classes, supporting trivial and hash-based directory structures.
    - Expand tests in `b_speed_test.py` to cover pickling utilities (`save_pickle` and `read_pickle`).
    - Enable push and pull_request triggers in GitHub Actions workflows (`c_static_checkout.yml` and `d_run_unit_tests.yml`).
    - Bump version to `0.3.0.18` in `pyproject.toml`.

--- tests/storage/table/b_speed_test.py
+++ tests/storage/table/b_speed_test.py
@@ -2,7 +2,7 @@ import os
 from pathlib import Path
 
 from gatling.storage.g_table.table_ao_file_tsv import TableAO_FileTSV
-from gatling.utility.io_fctns import save_jsonl, read_jsonl, remove_file
+from gatling.utility.io_fctns import save_jsonl, read_jsonl, remove_file, save_pickle, read_pickle
 from gatling.utility.watch import Watch
 from storage.table.a_const_test import rand_row, const_key2type
 
@@ -14,6 +14,7 @@ if __name__ == '__main__':
 
     fpath_temp_tsv = os.path.join(dtemp, 'temp.tsv')
     fpath_temp_jsonl = os.path.join(dtemp, 'temp.jsonl')
+    fpath_temp_pkl = os.path.join(dtemp, 'temp.pkl')
     remove_file(fpath_temp_tsv)
     remove_file(fpath_temp_jsonl)
 
@@ -29,6 +30,10 @@ if __name__ == '__main__':
     save_jsonl(rows, fpath_temp_jsonl)
     print(f"save_jsonl {w.see_timedelta()}")
 
+
+    save_pickle(rows,fpath_temp_pkl)
+    print(f"save_pickle {w.see_timedelta()}")
+
     ft = TableAO_FileTSV(fpath_temp_tsv).initialize(key2type=const_key2type)
     ft.extend(rows)
     print(f"FileTableAO.extend {w.see_timedelta()}")
@@ -36,6 +41,10 @@ if __name__ == '__main__':
     _ = read_jsonl(fpath_temp_jsonl)
     print(f"read_jsonl {w.see_timedelta()}")
 
+    _ = read_pickle(fpath_temp_pkl)
+    print(f"read_pickle {w.see_timedelta()}")
+
+    w = Watch()
     _ = ft[:]
     print(f"FileTableAO[:] {w.see_timedelta()}")
 
@@ -43,4 +52,5 @@ if __name__ == '__main__':
     _ = input("Press Enter to continue...")
     remove_file(fpath_temp_tsv)
     remove_file(fpath_temp_jsonl)
+    remove_file(fpath_temp_pkl)
 
